0c643c5d1fa1c4f2efca52f387e33397131b7366
Ilya Zhuravlev <ichirozcom@gmail.com>
2019-03-17 13:57:49 +0300

    service grid description

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,20 +8,20 @@ $(document).ready(() => {
 
   let content_slider = new Swiper('#content', {
     direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    shortSwipes: false,
     pagination: {
       el: '.swiper-pagination-v',
       clickable: true
     }
   });
 
-  $(document).on('wheel', (e) => {
-    let delta = e.originalEvent.deltaY;
-    if (delta > 0){
-      content_slider.slideNext();
-    } else {
-      content_slider.slidePrev();
-    }
-  });
+  $(window).on("load resize ", function() {
+    var scrollWidth = $('.table-content').width() - $('.table-content table').width();
+    $('.table-header').css({'padding-right':scrollWidth});
+  }).resize();
 
 
   // * Modals
@@ -54,8 +54,19 @@ $(document).ready(() => {
   });
 
   $('.see-comp').click((e) => {
-    console.log(e.target);
-    get_plan.setContent('<h1>Планировка дома</h1>');
+    const header = $(e.target).closest('.desc').children('.title').text();
+    if (header === 'Финский дом 33 м2') {
+      get_plan.setContent($('#see-comp-h33').html());
+    }
+    if (header === 'Финский дом 52 м2') {
+      get_plan.setContent($('#see-comp-h52').html());
+    }
+    if (header === 'Финский дом 64 м2') {
+      get_plan.setContent($('#see-comp-h64').html());
+    }
+    if (header === 'Финская баня 33 м2') {
+      get_plan.setContent($('#see-comp-b33').html());
+    }
     get_plan.open();
   });
 
@@ -73,8 +84,19 @@ $(document).ready(() => {
     see_more.close();
   });
   $('.read-more').click((e) => {
-    console.log(e.target);
-    see_more.setContent($('#see_more_template').html());
+    const header = $(e.target).closest('.desc').children('.title').text();
+    if (header === 'Финский дом 33 м2') {
+      see_more.setContent($('#see_more_template_h33').html());
+    }
+    if (header === 'Финский дом 52 м2') {
+      see_more.setContent($('#see_more_template_h52').html());
+    }
+    if (header === 'Финский дом 64 м2') {
+      see_more.setContent($('#see_more_template_h64').html());
+    }
+    if (header === 'Финская баня 33 м2') {
+      see_more.setContent($('#see_more_template_b33').html());
+    }
     see_more.open();
   });
 });
